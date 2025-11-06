d0d0d216cc5b30e1160fd4ad5c6dbb393199c57c
RPD Captial Finance <rpdcapitalfinancechennai@gmail.com>
2025-11-06 17:58:15 +0530

    Login and register error fixing

--- src/api.js
+++ src/api.js
@@ -1,5 +1,2 @@
 // src/api.js
-export const API =
-  process.env.NODE_ENV === "production"
-    ? "https://rpd-financial-backend.onrender.com/api"
-    : "http://localhost:3001";
+export const API = "https://rpd-financial-backend.onrender.com/api";
